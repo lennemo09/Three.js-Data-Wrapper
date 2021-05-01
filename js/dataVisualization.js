c014fa18c2324acfb3b8443cbeefc86984d9c3f6
Nhan Luong <lennemo09@gmail.com>
2021-05-01 20:07:01 +1000

    Added 3D axes and camera rotation with mouse.

--- js/dataVisualization.js
+++ js/dataVisualization.js
@@ -1,11 +1,12 @@
 //import * as THREE from 'three';
 //import { MeshLine, MeshLineMaterial, MeshLineRaycast } from './THREE.MeshLine';
 'use strict'
+
 var container = document.getElementById( 'container' );
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, .1, 1000 );
-camera.position.set( 0, 0, 10 );
+camera.position.set( 0, 0, 20 );
 
 var frustumSize = 1000;
 var resolution = new THREE.Vector2( window.innerWidth, window.innerHeight );
@@ -15,10 +16,12 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setPixelRatio( window.devicePixelRatio );
 container.appendChild( renderer.domElement );
 
-var clock = new THREE.Clock();
+var controls = new THREE.OrbitControls( camera, renderer.domElement );
 
-init()
-render();
+var clock = new THREE.Clock();
+var axesGroup;
+
+init();
 
 function drawLine(points, color, thickness=10) {
     const line = new MeshLine();
@@ -33,21 +36,64 @@ function drawLine(points, color, thickness=10) {
 		lineWidth: thickness,
     });
     
-    const mesh = new THREE.Mesh(line, material);
-    scene.add(mesh);
+    const lineMesh = new THREE.Mesh(line, material);
+
+    return lineMesh;
+}
+
+function drawCartesianAxes(axisLength = 4) {
+    let xAxisPoints = [axisLength,0,0,0,0,0];
+    let xAxis = drawLine(xAxisPoints,0x00ff00,5);
+
+    let yAxisPoints = [0,axisLength,0,0,0,0];
+    let yAxis = drawLine(yAxisPoints,0x0000ff,5);
+
+    let zAxisPoints = [0,0,axisLength,0,0,0];
+    let zAxis = drawLine(zAxisPoints,0xff0000,5);
+
+    axesGroup = new THREE.Group();
+    axesGroup.add(xAxis);
+    axesGroup.add(yAxis);
+    axesGroup.add(zAxis);
+
+    scene.add(axesGroup);
+    return axesGroup;
 }
 
 function init() {
-    let points = [];
-    points.push(0,4,0);
-    points.push(0,0,0);
-    let black = 0x000000
-    drawLine(points,black,5);
+    axesGroup = drawCartesianAxes(10);
+    render();
 }
+
+onWindowResize();
+
+function onWindowResize() {
+
+	var w = container.clientWidth;
+	var h = container.clientHeight;
+
+	var aspect = w / h;
+
+	camera.left   = - frustumSize * aspect / 2;
+	camera.right  =   frustumSize * aspect / 2;
+	camera.top    =   frustumSize / 2;
+	camera.bottom = - frustumSize / 2;
+
+	camera.updateProjectionMatrix();
+
+	renderer.setSize( w, h );
+
+	resolution.set( w, h );
+
+}
+
+window.addEventListener( 'resize', onWindowResize );
 
 function render() {
 
-	requestAnimationFrame( render );
+    requestAnimationFrame( render );
+    controls.update();
+
 	renderer.render( scene, camera );
 
 }
